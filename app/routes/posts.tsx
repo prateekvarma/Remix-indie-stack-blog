5c26536d254f9a828758ef32720a168213da294a
prateekvarma <prtk.varma@gmail.com>
2023-06-23 14:19:07 -0600

    Mocked data through the loader function and useLoaderData()

--- app/routes/posts.tsx
+++ app/routes/posts.tsx
@@ -1,7 +1,45 @@
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+
+export const loader = async () => {
+  const posts = [
+    {
+      slug: "my-first-post",
+      title: "My First Post",
+    },
+    {
+      slug: "my-second-post",
+      title: "My Second Post",
+    },
+  ];
+
+  //   const postsString = JSON.stringify({ posts });
+
+  //   return new Response(postsString, {
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   });
+  // the one line below is the equivalent of the above Response
+
+  return json({ posts });
+};
+
 export default function PostsRoute() {
-    return (
-        <main>
-            <h1>Posts</h1>
-        </main>
-    )
+  const { posts } = useLoaderData();
+
+  return (
+    <main>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
