c225db082635f327eb5ead66da609f8d201695e7
prateekvarma <prtk.varma@gmail.com>
2023-06-27 15:30:33 -0600

    Avoided over-fetching by confiuring the findMany in Prisma

--- app/routes/posts.tsx
+++ app/routes/posts.tsx
@@ -1,14 +1,14 @@
 import { json } from "@remix-run/node";
 import type { LoaderFunction } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
-import { getPosts } from "~/models/post.server";
+import { getPostListings } from "~/models/post.server";
 
 type LoaderData = {
   posts: Awaited<ReturnType<typeof getPosts>>;
 };
 
 export const loader: LoaderFunction = async () => {
-  const posts = await getPosts();
+  const posts = await getPostListings();
   return json<LoaderData>({ posts });
 };
 
