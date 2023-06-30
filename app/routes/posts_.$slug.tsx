e77879a329fe91c18b4f142d867781ccc3c4c78f
prateekvarma <prtk.varma@gmail.com>
2023-06-30 16:51:23 -0600

    Type checked LoaderData for posts

--- app/routes/posts_.$slug.tsx
+++ app/routes/posts_.$slug.tsx
@@ -4,6 +4,11 @@ import { useLoaderData } from "@remix-run/react";
 import { getPost } from "~/models/post.server";
 import invariant from "tiny-invariant";
 
+type LoaderData = {
+  title: string;
+  html: string;
+};
+
 export const loader: LoaderFunction = async ({ params }) => {
   const { slug } = params;
   invariant(slug, "slug is required");
@@ -11,14 +16,14 @@ export const loader: LoaderFunction = async ({ params }) => {
 
   invariant(post, `post not found: ${slug}`);
   const html = marked(post.markdown);
-  return json({ post, html });
+  return json<LoaderData>({ title: post.title, html });
 };
 
 export default function PostRoute() {
-  const { post, html } = useLoaderData();
+  const { title, html } = useLoaderData() as LoaderData;
   return (
     <main className="mx-auto max-w-4xl">
-      <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
+      <h1 className="my-6 border-b-2 text-center text-3xl">{title}</h1>
       <div dangerouslySetInnerHTML={{ __html: html }} />
     </main>
   );
