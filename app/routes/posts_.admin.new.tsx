d9001c6685a8cde2b50cc29c5f02b1d3fd8e0768
prateekvarma <prtk.varma@gmail.com>
2023-07-03 21:51:15 -0600

    Created form to create a new post

--- app/routes/posts_.admin.new.tsx
+++ app/routes/posts_.admin.new.tsx
@@ -1,3 +1,39 @@
+import { Form } from "@remix-run/react";
+
+const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
+
 export default function NewPostRoute() {
-  return <h2>New Post</h2>;
+  return (
+    <Form method="post">
+      <p>
+        <label>
+          Post Title:
+          <input type="text" name="title" className={inputClassName} />
+        </label>
+      </p>
+      <p>
+        <label>
+          Post Slug:
+          <input type="text" name="slug" className={inputClassName} />
+        </label>
+      </p>
+      <p>
+        <label>Markdown:</label>
+        <textarea
+          id="markdown"
+          rows={20}
+          name="markdown"
+          className={`${inputClassName} font-mono`}
+        />
+      </p>
+      <p className="text-right">
+        <button
+          type="submit"
+          className="bg-blue-500 px-4 py-2 text-white hover:to-blue-800"
+        >
+          Create Post
+        </button>
+      </p>
+    </Form>
+  );
 }
