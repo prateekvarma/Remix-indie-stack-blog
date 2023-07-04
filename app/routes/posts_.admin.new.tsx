2bd07426df26c5dbb7b29b34b11e71844e35719f
prateekvarma <prtk.varma@gmail.com>
2023-07-04 12:41:47 -0600

    Added validation for post creation

--- app/routes/posts_.admin.new.tsx
+++ app/routes/posts_.admin.new.tsx
@@ -1,5 +1,5 @@
-import { Form } from "@remix-run/react";
-import { redirect } from "@remix-run/node";
+import { Form, useActionData } from "@remix-run/react";
+import { json, redirect } from "@remix-run/node";
 import type { ActionFunction } from "@remix-run/node";
 import { createPost } from "~/models/post.server";
 
@@ -10,7 +10,19 @@ export const action: ActionFunction = async ({ request }) => {
   const slug = formData.slug;
   const markdown = formData.markdown;
 
-  await createPost({ title, slug, markdown })
+  const errors = {
+    title: title ? null : "title is required",
+    slug: slug ? null : "slug is required",
+    markdown: markdown ? null : "markdown is required",
+  };
+
+  const hasErrors = Object.values(errors).some((errorMessage) => errorMessage);
+
+  if (hasErrors) {
+    return json(errors);
+  }
+
+  await createPost({ title, slug, markdown });
 
   return redirect("/posts/admin");
 };
@@ -18,22 +30,31 @@ export const action: ActionFunction = async ({ request }) => {
 const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
 
 export default function NewPostRoute() {
+  const errors = useActionData();
+
   return (
     <Form method="post">
       <p>
         <label>
-          Post Title:
+          Post Title: { errors?.title ? (
+            <em className="text-red-600">{errors.title}</em>
+          ): null }
           <input type="text" name="title" className={inputClassName} />
         </label>
       </p>
       <p>
         <label>
-          Post Slug:
+          Post Slug: { errors?.slug ? (
+            <em className="text-red-600">{errors.slug}</em>
+          ): null }
           <input type="text" name="slug" className={inputClassName} />
         </label>
       </p>
       <p>
         <label>Markdown:</label>
+        { errors?.markdown ? (
+            <em className="text-red-600">{errors.markdown}</em>
+          ): null }
         <textarea
           id="markdown"
           rows={20}
