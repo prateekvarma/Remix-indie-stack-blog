8138b1893acec360245c450f8e7142373701e44c
prateekvarma <prtk.varma@gmail.com>
2023-07-05 16:32:07 -0600

    Added more type checks

--- app/routes/posts_.admin.new.tsx
+++ app/routes/posts_.admin.new.tsx
@@ -2,6 +2,15 @@ import { Form, useActionData } from "@remix-run/react";
 import { json, redirect } from "@remix-run/node";
 import type { ActionFunction } from "@remix-run/node";
 import { createPost } from "~/models/post.server";
+import invariant from "tiny-invariant";
+
+type ActionData =
+  | {
+      title: null | string;
+      slug: null | string;
+      markdown: null | string;
+    }
+  | undefined;
 
 export const action: ActionFunction = async ({ request }) => {
   const formData = Object.fromEntries(await request.formData());
@@ -10,7 +19,7 @@ export const action: ActionFunction = async ({ request }) => {
   const slug = formData.slug;
   const markdown = formData.markdown;
 
-  const errors = {
+  const errors: ActionData = {
     title: title ? null : "title is required",
     slug: slug ? null : "slug is required",
     markdown: markdown ? null : "markdown is required",
@@ -19,8 +28,12 @@ export const action: ActionFunction = async ({ request }) => {
   const hasErrors = Object.values(errors).some((errorMessage) => errorMessage);
 
   if (hasErrors) {
-    return json(errors);
+    return json<ActionData>(errors);
   }
+
+  invariant(typeof title === "string", "title must be a string");
+  invariant(typeof slug === "string", "slug must be a string");
+  invariant(typeof markdown === "string", "markdown must be a string");
 
   await createPost({ title, slug, markdown });
 
@@ -30,31 +43,33 @@ export const action: ActionFunction = async ({ request }) => {
 const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
 
 export default function NewPostRoute() {
-  const errors = useActionData();
+  const errors = useActionData() as ActionData;
 
   return (
     <Form method="post">
       <p>
         <label>
-          Post Title: { errors?.title ? (
+          Post Title:{" "}
+          {errors?.title ? (
             <em className="text-red-600">{errors.title}</em>
-          ): null }
+          ) : null}
           <input type="text" name="title" className={inputClassName} />
         </label>
       </p>
       <p>
         <label>
-          Post Slug: { errors?.slug ? (
+          Post Slug:{" "}
+          {errors?.slug ? (
             <em className="text-red-600">{errors.slug}</em>
-          ): null }
+          ) : null}
           <input type="text" name="slug" className={inputClassName} />
         </label>
       </p>
       <p>
         <label>Markdown:</label>
-        { errors?.markdown ? (
-            <em className="text-red-600">{errors.markdown}</em>
-          ): null }
+        {errors?.markdown ? (
+          <em className="text-red-600">{errors.markdown}</em>
+        ) : null}
         <textarea
           id="markdown"
           rows={20}
