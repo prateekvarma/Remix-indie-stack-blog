99293dc3dcbed41cab7bc3206281c2405739847d
prateekvarma <prtk.varma@gmail.com>
2023-07-07 14:13:11 -0600

    Displaying ENV data on client and server

--- app/root.tsx
+++ app/root.tsx
@@ -8,21 +8,35 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 
 import { getUser } from "~/session.server";
 import stylesheet from "~/tailwind.css";
+import { getEnv } from "./env.server";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
+type LoaderData = {
+  user: Awaited<ReturnType<typeof getUser>>;
+  ENV: ReturnType<typeof getEnv>;
+};
+
 export const loader = async ({ request }: LoaderArgs) => {
-  return json({ user: await getUser(request) });
+  return json<LoaderData>({
+    user: await getUser(request),
+    ENV: getEnv(),
+  });
 };
 
 export default function App() {
+  console.log("Admin email: ", ENV);
+
+  const data = useLoaderData()
+
   return (
     <html lang="en" className="h-full">
       <head>
@@ -35,6 +49,11 @@ export default function App() {
         <Outlet />
         <ScrollRestoration />
         <Scripts />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.ENV = ${JSON.stringify(data.ENV)}`,
+          }}
+        ></script>
         <LiveReload />
       </body>
     </html>
